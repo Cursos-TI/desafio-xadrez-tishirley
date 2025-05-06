442ce9b965e1fcf3b8495f5a029585c226ce906b
tishirley <vipconvitesjf@gmail.com>
2025-05-06 17:28:05 +0000

    do while

--- xadrez.c
+++ xadrez.c
@@ -14,5 +14,15 @@ int main() {
         printf("\n");
     }
 
+    printf("Bispo\n");
+
+    do { //Entrada (1x pelo menos)
+        printf("%d\n", 1); //Saída
+        i++; //Incremento
+    } while (i <= 5); //Condição
+
+    printf("Cima, Direita\n");
+    printf("\n");
+
     return 0;
 }
